16b2cffa958321463ab772485dfd4c07084cff76
ryankim2731 <44594374+ryankim2731@users.noreply.github.com>
2019-10-26 09:29:00 -0700

    Add files via upload

---
+++ data_refactoring.py
@@ -0,0 +1,24 @@
+from datetime import date
+from gs_quant.data import Dataset
+from gs_quant.markets.securities import SecurityMaster, AssetIdentifier
+from gs_quant.session import GsSession
+
+client_id = "b2a0a48a91854045b5c7eb12c8973901"
+client_secret = "2cb4ec68204ddee53a1a892ea70ee110bc5db9c30aa04024336d69ea9e4fafc6"
+
+scopes = GsSession.Scopes.get_default()
+GsSession.use(client_id = client_id, client_secret = client_secret, scopes = scopes)
+
+ds = Dataset("USCANFPP_MINI")
+
+gsids = ds.get_coverage()["gsid"].values.tolist()
+data = ds.get_data(date(2017, 1, 15), date(2018, 1, 15), gsids=gsids[0:5])
+
+print(data.head())
+
+for idx, row in data.iterrows():
+    marqueeAssetId = row["assetId"]
+    asset = SecurityMaster.get_asset(marqueeAssetId, AssetIdentifier.MARQUEE_ID)
+    data.loc[data["assetId"] == marqueeAssetId, "assetName"] = asset.name
+
+print(data.head())
