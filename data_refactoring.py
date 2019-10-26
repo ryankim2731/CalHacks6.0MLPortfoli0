2fc8fe5495c5ef78fc110d44b48bad8c5e051a7a
mdmartel <47866900+mdmartel@users.noreply.github.com>
2019-10-26 12:28:11 -0700

    Data Refactoring and Raw Data
    Need to change the file open path to the Raw Data location

--- data_refactoring.py
+++ data_refactoring.py
@@ -3,22 +3,94 @@ from gs_quant.data import Dataset
 from gs_quant.markets.securities import SecurityMaster, AssetIdentifier
 from gs_quant.session import GsSession
 
-client_id = "b2a0a48a91854045b5c7eb12c8973901"
-client_secret = "2cb4ec68204ddee53a1a892ea70ee110bc5db9c30aa04024336d69ea9e4fafc6"
+class Stock:
+	def __init__(self):
+		self.needs_name = True
+		self.name = ""
+		self.risk = []
+		self.growth = []
+		self.returns = []
+		self.multiple = []
+		self.factor = []
 
-scopes = GsSession.Scopes.get_default()
-GsSession.use(client_id = client_id, client_secret = client_secret, scopes = scopes)
+	def add_risk(self, risk_val):
+		self.risk.append(risk_val)
 
-ds = Dataset("USCANFPP_MINI")
 
-gsids = ds.get_coverage()["gsid"].values.tolist()
-data = ds.get_data(date(2017, 1, 15), date(2018, 1, 15), gsids=gsids[0:5])
+	def rename(self, name):
+		self.name = name
+	def add_growth(self, growth_val):
+		self.growth.append(growth_val)
+	def add_returns(self, returns_val):
+		self.returns.append(returns_val)
+	def add_multiple(self, multiple_val):
+		self.multiple.append(multiple_val)
+	def add_factor(self, factor_val):
+		self.factor.append(factor_val)
+	def add_all(self, growth_val, returns_val, multiple_val, factor_val):
+		add_growth(growth_val)
+		add_returns(returns_val)
+		add_multiple(multiple_val)
+		add_factor(factor_val)		
 
-print(data.head())
+yeet = open('C:/Users/User/Desktop/Hackathons/CalHacks6.0/CalHacks6.0MLPortfoli0-master/data_raw.txt', 'r')
+identifiers = ["gsid", "assetId", "financialReturnsScore", "growthScore", "multipleScore", "integratedScore", "updateTime", "assetName", "date"]
 
-for idx, row in data.iterrows():
-    marqueeAssetId = row["assetId"]
-    asset = SecurityMaster.get_asset(marqueeAssetId, AssetIdentifier.MARQUEE_ID)
-    data.loc[data["assetId"] == marqueeAssetId, "assetName"] = asset.name
+stocks = []
 
-print(data.head())
+current_line = (yeet.read().split())
+
+cycle_count = 0
+line_count = 0
+stock_index = 0
+stock_name = ""
+for i in current_line:
+	print(i)
+	if i in identifiers:
+		stock_name = ""
+		cycle_count = 0
+	else:
+		if cycle_count % 9 == 0:
+			if i == "2017-01-16":
+				stocks.append(Stock())
+				if len(stocks) > 1:
+					stock_index += 1
+
+		if cycle_count % 9 == 3:
+			stocks[stock_index].add_returns(i)
+		if cycle_count % 9 == 4:
+			stocks[stock_index].add_growth(i)
+		if cycle_count % 9 == 5:
+			stocks[stock_index].add_multiple(i)	
+		if cycle_count % 9 == 6:
+			stocks[stock_index].add_factor(i)								
+		if cycle_count % 9 == 8:
+			if i[0:4] != "2017":
+				stock_name += i + " "
+				cycle_count -= 1
+			else:
+				cycle_count += 1
+				if stocks[stock_index].needs_name == True:
+					stocks[stock_index].rename(stock_name)
+					stocks[stock_index].needs_name = False
+		cycle_count += 1
+		#sprint("cycle_count = "+ str(cycle_count))
+
+
+print()
+print()
+print("Number of stocks: " + str(len(stocks)))
+print()
+
+for i in stocks:
+	print(i.name)
+	print("Growth")
+	print(i.growth)
+	print("Returns")
+	print(i.returns)
+	print("Multiple")
+	print(i.multiple)
+	print("Factor")
+	print(i.factor)
+	print("-----------------\n")
+print(str(cycle_count))	
